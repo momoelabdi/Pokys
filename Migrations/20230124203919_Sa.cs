c1d9e2fe50800fdd3de55ad3bf988d8718216fa6
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2023-01-24 21:41:26 +0100

    mysql connection established

---
+++ Migrations/20230124203919_Sa.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pokys.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
