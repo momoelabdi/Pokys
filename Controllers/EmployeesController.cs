947378865bc5dce88acf23e9ecdb261fb2205a74
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2023-01-26 20:54:20 +0100

    first crudss

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,15 +1,39 @@
 
 using Microsoft.AspNetCore.Mvc;
-
+using Pokys.Models;
+using Pokys.Data;
+using Pokys.Models.Domain;
 namespace Pokys.Controllers
 {
   public class EmployeesController : Controller
   {
+  private readonly MVCDemoDbContext mvcDemoDbContext;
+    public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+    {
+      this.mvcDemoDbContext = mvcDemoDbContext;
+    }
 
     [HttpGet]
     public IActionResult Add()
     {
       return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+    {
+      var employee = new Employee()
+      {
+        Id = Guid.NewGuid(),
+        Name = addEmployeeRequest.Name,
+        Email = addEmployeeRequest.Email,
+        Salary = addEmployeeRequest.Salary,
+        Departement = addEmployeeRequest.Departement,
+        DateOfBirth = addEmployeeRequest.DateOfBirth
+      };
+      await mvcDemoDbContext.Employees.AddAsync(employee);
+      await mvcDemoDbContext.SaveChangesAsync();
+      return RedirectToAction("Add");
     }
 
   }
--- Models/Domain/Employee.cs
+++ Models/Domain/Employee.cs
@@ -3,6 +3,7 @@ namespace Pokys.Models.Domain
 {
   public class Employee
   {
+    public Guid Id { get; set;}
     public string Name { get; set;}
     public string Email { get; set;}
     public long Salary { get; set;}
